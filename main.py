18fe40e930fb2768d6a8667068883e70c553d865
CoryARamirez <coryaramirez@gmail.com>
2019-04-07 23:22:51 +0000

    Factor out SQL query, add .gitignore, update main with all three required SQL calls & add example output in example.txt

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 import psycopg2
-
-def query(statement, conn):
-    try:
-        # Instantiate a cursor that will be used to interact with the db
-        db_cursor = conn.cursor()
-        
-    except Exception as e:
-        print("Cursor error:", e)
-        exit()
-        
-    # Execute query statement
-    db_cursor.execute(statement)
-    
-    results = db_cursor.fetchall()
-    
-    db_cursor.close()
-    
-    return results
-    
+from sql import query
 
 def main(connection):
     print("Setting up Udacity Log Analysis Program\nRE: FSND - Project 1 - Cory A. Ramirez")
     
+    ############################################################################
     # What articles have been accessed the most of all time?
     try:
         results = query("""SELECT articles.title, COUNT(*)
@@ -39,10 +22,18 @@ def main(connection):
     
     for i, record in enumerate(results):
         print("Rank " + str(i + 1) + ": " + record[0] + " with " + str(record[1]) + " views.")
+        
+    ############################################################################
     
     # Which authors got the most page views?
     try:
-        results = query("""SELECT * from articles""", connection)
+        results = query("""SELECT authors.name, count (*)
+            AS results from log, authors, articles
+            WHERE log.status = '200 OK'
+            AND articles.slug = substr(log.path, 10)
+            AND authors.id = articles.author
+            GROUP BY authors.name
+            ORDER BY results desc""", connection)
         
     except Exception as e:
         print("Query error:", e)
@@ -50,9 +41,27 @@ def main(connection):
     print("Authors with the most page views: ")
     
     for i, record in enumerate(results):
-        print(record)
+        print("Rank " + str(i + 1) + ": " + record[0] + " with " + str(record[1]) + " views.")
+        
+    ############################################################################
 
     # Which days had greater than 1% of HTTP errors?
+    try:
+        results = query("""SELECT errors.date, errors.err/errors.total * 100
+        AS percentage
+        FROM (SELECT cast(time AS date) as date,
+        COUNT(*) AS total,
+        CAST(sum(cast(status  != '200 OK' as int)) AS float) AS err
+        FROM log
+        GROUP BY date) AS errors
+        WHERE errors.err/errors.total > 0.01;""", connection)
+        
+    except Exception as e:
+        print("Query error:", e)
+        
+    print("Days with HTTP errors larger than 1% of all errors: ")
+    
+    print("Date: " + str(results[0][0]) + " with number of errors totaling " + "{0:0.1f}".format(results[0][1]) + "%.")
     
     # Close db connection
     db_conn.close()
